dbc808380ce3d79a6bf0fdb87a3da6ee0719d370
Chun-Hui Gao <gaospecial@gmail.com>
2021-04-14 21:37:49 +0800

    save shape data

--- data-raw/shapes.R
+++ data-raw/shapes.R
@@ -2,61 +2,76 @@
 
 # construct a sf object for ploting Venn
 # columns in `shapes`
-# - shape_id: shape id, each shape has three components, and belongs to different type
 # - nsets: number of sets, 1:7
-# - component: c("setEdge","setLabel","region")
-# - type: c("ellipses","triangle","polygon")
-# - no: No. of shape
-# - id: id of the regions of shape
-# - geometry: coordination of regions
-library(sf)
+# - type: c("ellipse","triangle","polygon", "circle")
+# - shape_id: shape id, each shape has three components, and belongs to different type
+# - component: c("setEdge","setLabel")
+# - id: id of set/region
+# - xy: coordination of regions
 
-# 4d ellipses
-parameters <- list(c(0.35, 0.47, 0.35, 0.20, 135),
-                   c(0.50, 0.57, 0.35, 0.15, 135),
-                   c(0.50, 0.57, 0.33, 0.15,  45),
-                   c(0.65, 0.47, 0.35, 0.20,  45))
-ellipses <- lapply(parameters,function(x){
-  a <- VennDiagram::ell2poly(x[[1]],x[[2]],x[[3]],x[[4]],x[[5]],n.sides = 3000) %>%
-    data.frame()
-  rbind(a, a[1,])
-})
+# 4d ellipse
+f4e <- build_shape(
+  edge = fancy_4d_ellipse(),
+  label = fancy_4d_ellipse_label(),
+  shape_id = "401f",
+  type = "ellipse"
+)
 
 
-label_position <- data.frame(x = c(0.08, 0.26, 0.71, 0.93),
-                             y = c(0.78, 0.86, 0.85, 0.78))
-points <- lapply(seq_len(nrow(label_position)),function(i){
-  st_point(as.matrix(label_position[i,]))
-})
+# 3d circle
+f3c <- build_shape(
+  edge = fancy_3d_circle(),
+  label = fancy_3d_circle_label(),
+  shape_id = "301f",
+  type = "circle"
+)
 
-category_labels <- tibble::tibble(
-  component = "setLabel",
-  id    = as.character(seq_len(length(ellipses))),
-  geometry = points
+# 2d circle
+f2c <- build_shape(
+  edge = fancy_2d_circle(),
+  label = fancy_2d_circle_label(),
+  shape_id = "201f",
+  type = "circle"
+)
+
+# 6d triangle
+f6t <- build_shape(
+  edge = fancy_6d_triangle(),
+  label = fancy_6d_triangle_label(),
+  shape_id = "601f",
+  type = "triangle"
+)
+
+# import venn:::sets dataset
+transform_venn_data <- function(sets){
+  sets <- sets %>%
+    dplyr::filter(!is.na(x), !is.na(y)) %>%
+    tidyr::nest(xy = c(x,y)) %>%
+    dplyr::mutate(type = ifelse(v == 1, "ellipse", "polygon"),
+           nsets = s,
+           shape_id = paste(s,v+1,sep = "0"),
+           id = n,
+           xy = as.matrix(xy)) %>%
+    dplyr::select(nsets, type, shape_id, id, xy)
+  sets$xy <- lapply(sets$xy, as.matrix)
+  sets
+}
+sets <- venn:::sets
+sets <- transform_venn_data(sets) %>% dplyr::mutate(component = "setEdge")
+
+# label position indicated by venn::venn()
+scoords <- data.frame(
+  s = c(1, rep(2, 2), rep(3, 3), rep(4, 4), rep(5, 10), rep(6, 6), rep(7, 7), rep(4, 4)),
+  v = c(rep(0, 1 + 2 + 3), rep(1, 4), rep(0:1, each = 5), rep(0, 6 + 7), rep(0, 4)),
+  x = c(500, 250, 750, 100, 500, 900,  88, 263, 713, 888,     80, 535, 900, 700, 120,      88, 533, 850, 750, 163,       100, 500, 910, 925, 550, 100, 220, 685, 935, 935, 600, 155,  50,  85, 220, 780, 915),
+  y = c(780, 780, 780, 560, 910, 560, 663, 850, 850, 663,    800, 960, 700,  50, 120,     750, 963, 688,  40,  88,       860, 975, 775, 165,  30, 140, 955, 980, 780, 200,  15, 120, 690, 670, 850, 850, 670)
 ) %>%
-  st_as_sf()
+  dplyr::group_by(s,v) %>%
+  dplyr::mutate(n=dplyr::row_number()) %>%
+  dplyr::ungroup()
+scoords <- transform_venn_data(scoords) %>% dplyr::mutate(component = "setLabel")
 
-linestrings <- lapply(ellipses, function(x) st_linestring(as.matrix(x)))
-sets <- tibble::tibble(
-  component = "setEdge",
-  id = as.character(seq_len(length(ellipses))),
-  geometry = linestrings
-) %>%
-  st_as_sf()
+shapes <- dplyr::bind_rows(f4e, f3c, f2c, f6t, sets, scoords)
 
-# region polygons
-polygons <- lapply(ellipses,function(x)st_polygon(list(as.matrix(x))))
-polygon <- Polygon(polygons)
-regions <- get_region_items(polygon)
-region_id <- get_region_ids(polygon)
-regions <- tibble::tibble(
-  component = "region",
-  id = region_id,
-  geometry = regions
-) %>%
-  st_as_sf()
-
-shapes <- rbind(sets, regions, category_labels) %>%
-  mutate(nsets = 4, type = "ellipse", shape_id = 1)
-
+# shapes <- NULL
 usethis::use_data(shapes, overwrite = TRUE, internal = TRUE)
