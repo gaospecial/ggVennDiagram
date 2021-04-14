0086b669b72af88d89da0a7aa2c8f92b659704b4
Chun-Hui Gao <gaospecial@gmail.com>
2021-04-14 18:25:59 +0800

    build shape with edge and label

---
+++ R/build_shape.R
@@ -0,0 +1,51 @@
+#' Helper function to add shape
+#'
+#' @param edge
+#' @param nsets
+#' @param type
+#' @param shape_id
+#' @param component
+#'
+#' @return
+#' @export
+#'
+#' @examples
+build_shape <- function(edge, label,
+                      nsets = length(edge),
+                      shape_id = "auto",
+                      type = c("ellipse","triangle","polygon","circle")){
+
+  if (sum(sapply(edge, is.matrix) == FALSE) >= 1)
+    stop("The element in edge must be a two-column matrix.")
+  if (sum(sapply(label, is.matrix) == FALSE) >= 1)
+    stop("The element in edge must be a two-column matrix.")
+  if (length(edge) != length(label))
+    stop("Length of edge/label must be equal.")
+  if (!is.list(edge) | !is.list(edge))
+    stop("edge/label must be a list.")
+  if (shape_id == "auto")
+    shape_id <- as.character(max(1, as.numeric(shapes$shape_id)) + 1)
+  if (shape_id %in% unique(shapes$shape_id))
+    stop("Your shape_id is duplicated with internal data.")
+  type <- match.arg(type)
+
+  shape_edge <- tibble::tibble(
+    nsets = nsets,
+    type = type,
+    shape_id = shape_id,
+    component = "setEdge",
+    id = seq_len(nsets),
+    xy = edge
+  )
+
+  shape_label <- tibble::tibble(
+    nsets = nsets,
+    type = type,
+    shape_id = shape_id,
+    component = "setLabel",
+    id = seq_len(nsets),
+    xy = label
+  )
+
+  rbind(shape_edge, shape_label)
+}
