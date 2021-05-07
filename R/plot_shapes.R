da990be7a4e2a5f79b6466972ced53bdd44a5d6d
Chun-Hui Gao <gaospecial@gmail.com>
2021-05-07 12:27:30 +0800

    plot shapes with base

--- R/plot_shapes.R
+++ R/plot_shapes.R
@@ -16,14 +16,15 @@
 #' @examples
 #' plot_shapes()
 plot_shapes <- function(){
-  edge <- shapes %>%
-    dplyr::filter(.data$component == "setEdge", .data$type %in% c("ellipse","circle","polygon"))
-  edge$xy <- lapply(edge$xy, dplyr::as_tibble)
-  edge %>% tidyr::unnest(.data$xy) %>%
-    dplyr::mutate_at("id",as.character) %>%
-    dplyr::rowwise() %>%
-    ggplot(aes_string(x="x",y="y",fill="id",group="id",color="id")) +
-    geom_path(size=1,alpha=0.5,show.legend = F) +
-    facet_wrap(~shape_id, scales = "free") +
-    theme_void()
+  ids <- sort(unique(shapes$shape_id))
+  ids <- ids[ids != "601f"]
+  # windows()
+  par(mfrow = c(3,4), mar = c(1,1,1,1))
+  success <- lapply(seq_along(ids), function(i){
+    id = ids[[i]]
+    shapes %>% dplyr::filter(.data$component == "setEdge", .data$shape_id == {{id}}) %>%
+      dplyr::pull(.data$xy) %>%
+      sf::st_polygon() %>%
+      plot(main = {{id}})
+  })
 }
