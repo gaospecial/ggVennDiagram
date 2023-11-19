99e4f5a571f44ee386737ec771d4e2116173a9d6
Chun-Hui Gao <gaospecial@gmail.com>
2023-11-19 10:25:21 +0800

    add disern_overlap()

--- R/setOperations.R
+++ R/setOperations.R
@@ -137,3 +137,46 @@ setMethod("discern", c(venn = "Venn", slice1 = "ANY", slice2 = "ANY"),
             differ
           }
 )
+
+
+# Method for specific region =============
+
+#' Calculate region of polygons
+#'
+#' calculate the unique region defined
+#' by `Venn` object and the parameter `slice`.
+#'
+#' @param polygon a Polygon object
+#' @param slice index of Venn members, default is "all"
+#'
+#' @return region items
+#' @export
+#' @name discern_overlap
+#' @author gaospecial@gmail.com
+#'
+#' @examples
+#' library(ggVennDiagram)
+#' venn <- Venn(list(A=1:3,B=2:5,C=c(1L,3L,5L)))
+#'
+#' discern_overlap(venn, slice = "all")
+#' # is equal to
+#' overlap(venn, slice = "all")
+#'
+#' # however, `discern_overlap()` only contains specific region
+#' discern_overlap(venn, slice = 1:2)
+#'
+setGeneric("discern_overlap", function(venn, slice = "all") standardGeneric("discern_overlap"))
+
+#' @export
+#' @rdname discern_overlap
+setMethod("discern_overlap", c(venn="Venn", slice="ANY"),
+          function(venn, slice = "all"){
+            overlap = overlap(venn, slice = slice)
+            if (slice[1] == "all" | identical(venn@sets[slice], venn@sets)){
+              discern = NULL
+              return(overlap)
+            } else {
+              discern = discern(venn, slice1 = slice, slice2 = "all")
+              return(intersect(overlap, discern))
+            }
+          })
