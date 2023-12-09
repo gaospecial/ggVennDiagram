9aa65718f11e87c0bc2d387b7de6ae2a4a5c919d
Chun-Hui Gao <gaospecial@gmail.com>
2023-12-09 22:44:25 +0800

    test class Venn

--- tests/testthat/test-class-Venn.R
+++ tests/testthat/test-class-Venn.R
@@ -1,8 +1,9 @@
-test_that("discern_overlap(Venn) works", {
+test_that("Venn methods work", {
   sets <- list(A=1:5,B=3:9,C=c(1,3,5,7,9,11))
   sets <- lapply(sets, as.character)
-  venn <- RVenn::Venn(sets = sets)
+  venn <- Venn(sets = sets)
 
-  expect_equal( discern_overlap(venn), c("3","5"))
-
+  expect_equal(discern_overlap(venn), c("3","5"))
+  expect_equal(discern_overlap(venn,1:2), c("4"))
+  expect_equal(overlap(venn), c("3","5"))
 })
--- tests/testthat/test-discern_overlap.R
+++
@@ -1,5 +0,0 @@
-test_that("discern_overlap(Venn) works", {
-  x <- list(A=1:3,B=3:7,C=2:9)
-  venn <- RVenn::Venn(x)
-  expect_equal(discern_overlap(venn,1:3), RVenn::overlap(venn))
-})
