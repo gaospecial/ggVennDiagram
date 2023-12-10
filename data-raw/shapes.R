bae2a47510c01f23ba2a6942f64f1634e73cbc35
Chun-Hui Gao <gaospecial@gmail.com>
2023-12-10 13:58:03 +0800

    regenerate shape data

--- data-raw/shapes.R
+++ data-raw/shapes.R
@@ -1,26 +1,23 @@
 ## code to prepare `shapes` dataset goes here
 
 rm(list = ls())
-if (!requireNamespace("shapeMageR", quietly = TRUE)){
-  devtools::load_all("../shapeMageR/")
-}
+devtools::install_github("gaospecial/shapeMageR")
+library(shapeMageR)
 
 ## code to prepare `ggVennDiagramShapes` dataset goes here
 
-## code to prepare `shapes` dataset goes here
-
-# construct a sf object for plotting Venn Diagram
+# construct a object for plotting Venn Diagram
 # columns in `shapes`
 # - shapeId: shape id, each shape has three components, and belongs to different type
 # - type: c("ellipse","triangle","polygon", "circle")
 # - nsets: number of sets, 1:7
-# - component: c("setEdge","setLabel")
+# - component: c("setEdge","setLabel","regionEdge","regionLabel")
 #   - id: id of set/region
-#   - geometry: coordination of regions
+#   - x, y: coordination of edges/labels
 
 
 # 4d ellipse
-f4e = VennPlotData(
+f4e = VennShape(
   shapeId = "401f",
   type = "ellipse",
   setEdge = fancy_4d_ellipse(),  # how to store object in data.frame
@@ -28,21 +25,21 @@ f4e = VennPlotData(
 
 
 # 3d circle
-f3c = VennPlotData(
+f3c = VennShape(
   setEdge = fancy_3d_circle(),
   setLabel =  fancy_3d_circle_label(),
   shapeId = "301f",
   type = "circle")
 
 # 2d circle
-f2c = VennPlotData(
+f2c = VennShape(
   setEdge = fancy_2d_circle(),
   setLabel =  fancy_2d_circle_label(),
   shapeId = "201f",
   type = "circle")
 
 # 6d triangle
-f6t = VennPlotData(
+f6t = VennShape(
   setEdge = fancy_6d_triangle(),
   setLabel =  fancy_6d_triangle_label(),
   shapeId = "601t",
@@ -65,7 +62,7 @@ scoords = data.frame(
         750,163,100,500,910,925,550,100,220,685,935,935,600,155,50,85,220,780,915),
   y = c(780,780,780,560,910,560,663,850,850,663,800,960,700,50,120,750,963,688,
         40,88,860,975,775,165,30,140,955,980,780,200,15,120,690,670,850,850,670)
-) %>%
+) |>
   dplyr::mutate(shapeId = paste(nsets, shapeId + 1, sep = "0"),
                 id = dplyr::row_number(),
                 .by = c(nsets, shapeId))
@@ -88,13 +85,22 @@ venn_shapes = lapply(seq_len(nrow(sets)), function(i){
   id = sets$shapeId[[i]]
   type = sets$type[[i]]
   edge = sets$geometry[[i]][["xy"]]
+  edge = lapply(edge, as.matrix)
   label =  scoords$geometry[[which(scoords$shapeId == id)]][["xy"]]
-  VennPlotData(setEdge = edge, setLabel = label, shapeId = id, type = type)
+  label = lapply(label, as.matrix)
+  VennShape(setEdge = edge, setLabel = label, shapeId = id, type = type)
 })
 
 shapes =
   c(list(f4e), list(f3c), list(f2c), list(f6t), venn_shapes)
-shapes = lapply(shapes, unclass)
+shapes = lapply(shapes, function(x){
+  tryCatch(unclass(x), error = function(e) NULL)
+})
+shapes = shapes[!sapply(shapes, is.null)]
 sortby = sapply(shapes, get_shape_nsets) |> order()
 shapes = shapes[sortby]
+shapes = lapply(shapes, function(x){
+  class(x) = "VennPlotData"
+  return(x)
+})
 usethis::use_data(shapes, overwrite = TRUE, internal = TRUE)
