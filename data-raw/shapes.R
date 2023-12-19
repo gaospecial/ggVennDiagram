69ab4028ac74dd831dd4c64a6c4a2ae0e995e200
Chun-Hui Gao <gaospecial@gmail.com>
2023-12-19 12:09:50 +0800

    update venn shapes

--- data-raw/shapes.R
+++ data-raw/shapes.R
@@ -55,15 +55,9 @@ sets = sets |>
   dplyr::mutate(shapeId = paste(nsets, shapeId + 1, sep = "0"))
 
 # label position indicated by venn::venn()
-scoords = data.frame(
-  nsets = c(1,rep(2,2),rep(3,3),rep(4,4),rep(5,10),rep(6,6),rep(7,7),rep(4,4)),
-  shapeId = c(rep(0, 1 + 2 + 3),rep(1, 4),rep(0:1, each = 5), rep(0, 6 + 7), rep(0, 4)),
-  x = c(500,250,750,100,500,900,88,263,713,888,80,535,900,700,120,88,533,850,
-        750,163,100,500,910,925,550,100,220,685,935,935,600,155,50,85,220,780,915),
-  y = c(780,780,780,560,910,560,663,850,850,663,800,960,700,50,120,750,963,688,
-        40,88,860,975,775,165,30,140,955,980,780,200,15,120,690,670,850,850,670)
-) |>
-  dplyr::mutate(shapeId = paste(nsets, shapeId + 1, sep = "0"),
+scoords = venn:::scoords
+colnames(scoords) = c("nsets", "shapeId", "x", "y")
+scoords = scoords |> dplyr::mutate(shapeId = paste(nsets, shapeId + 1, sep = "0"),
                 id = dplyr::row_number(),
                 .by = c(nsets, shapeId))
 
